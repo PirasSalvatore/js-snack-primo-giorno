cee31cd516f0a7f82e6a9278be53012f9cbd991c
PirasSalvatore <torep.92@gmail.com>
2025-02-13 11:33:26 +0100

    snaks 2 remove isNan controll

--- snacks2/script.js
+++ snacks2/script.js
@@ -4,12 +4,14 @@ Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
 //data user
-const wordOne = String(prompt("inserisci la prima parola"))
-const wordtwo = String(prompt("inserisci la seconda parola"))
+const wordOne = prompt("inserisci la prima parola")
+const wordtwo = prompt("inserisci la seconda parola")
 
-if (isNaN(wordOne) || isNaN(wordtwo)) {
+/* if (isNaN(wordOne) || isNaN(wordtwo)) {
     alert("invalid data")
-} else if (wordOne.length > wordtwo.length) {
+} else */
+
+if (wordOne.length > wordtwo.length) {
     console.log(`la parola ${wordtwo} è più corta di ${wordOne}`);
 } else if (wordOne.length < wordtwo.length) {
     console.log(`la parola ${wordOne} è più corta di ${wordtwo}`);
